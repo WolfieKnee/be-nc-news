079dd1a6c657e56bd7bbc1d15c85d78d0125cec6
WolfieKnee <david@davidwolfson.co.uk>
2024-01-15 12:04:53 +0000

    this will add completed task 2

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ const app = express();
 
 app.get("/api/topics", getTopics);
 
+app.use((err, req, res, next) => {
+	console.log(err);
+	res.status(500).send({
+		message: "500 error! sorry, Dr Dev made a boo-boo",
+	});
+});
+
 module.exports = app;
