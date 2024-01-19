d080ad57ff3d00b7b7604fbb99460a9ced66e306
WolfieKnee <david@davidwolfson.co.uk>
2024-01-19 11:16:30 +0000

    this resolves merge conflict by adding POST articles to routes

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -2,6 +2,7 @@ const {
 	getArticles,
 	getArticleById,
 	patchArticleByArticleId,
+	postArticle,
 } = require("../controllers/articles.controllers");
 const {
 	getCommentsByArticleId,
@@ -10,7 +11,7 @@ const {
 
 const articlesRouter = require("express").Router();
 
-articlesRouter.route("/").get(getArticles);
+articlesRouter.route("/").get(getArticles).post(postArticle);
 
 articlesRouter
 	.route("/:article_id")
