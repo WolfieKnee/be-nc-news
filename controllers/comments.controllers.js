9cae97083a39eda9581557faa40ac9fc652835c7
WolfieKnee <david@davidwolfson.co.uk>
2024-01-19 11:49:59 +0000

    this will add completed ticket 18

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -2,6 +2,7 @@ const {
 	fetchCommentsByArticleId,
 	insertCommentByArticleId,
 	removeCommentById,
+	updateCommentById,
 } = require("../models/comments.models");
 
 exports.getCommentsByArticleId = (req, res, next) => {
@@ -32,4 +33,14 @@ exports.deleteCommentById = (req, res, next) => {
 	removeCommentById(comment_id)
 		.then(() => res.status(204).send())
 		.catch((err) => next(err));
+};
+
+exports.patchCommentById = (req, res, next) => {
+	const { comment_id } = req.params;
+	const newVote = req.body.inc_votes;
+	updateCommentById(comment_id, newVote)
+		.then((comment) => res.status(201).send({ comment }))
+		.catch((err) => {
+			next(err);
+		});
 };
