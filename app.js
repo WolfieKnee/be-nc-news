ec4d2233531cdfbe60bf3d310c13fd4a72cee6ba
WolfieKnee <david@davidwolfson.co.uk>
2024-02-05 08:24:14 +0000

    this will add CORS dependancy

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const apiRouter = require("./routes/api-router");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
